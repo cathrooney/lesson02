c98e0d6258f686110e18464920666fcd8263936f
Cath Rooney <cathrooney@gmail.com>
2012-09-26 23:10:29 +0100

    submitting lab results

--- lab.rb
+++ lab.rb
@@ -1,6 +1,12 @@
 # declare a hash representing the four suites of a deck of cards
 # with an abbreviation for the keys and a full name for the values
-
+family = {h: "Hearts", s: "Spades", c: "Clubs", d: "Diamonds"}
 
 # display the rank and suit for each card, as "9 of Hearts", etc.
 # don't worry about "jack" or "king" yet, just go up to "13 of Hearts", etc.
+family.each do |code, name|
+	(1..13).each do |num|
+ 		puts "#{num} of #{name}"
+	end
+
+end
