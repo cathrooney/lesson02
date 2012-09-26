ac3a551b8133624714f914155fbd0e528d0bbbf6
Cath Rooney <cathrooney@gmail.com>
2012-09-26 20:02:25 +0100

    adding answers

--- arrays.rb
+++ arrays.rb
@@ -1,8 +1,19 @@
 # 1. create an array with three different string values
+jungle = ["elephant","rhino","leopard"]
 
 # 2. add two new values to the Task #1 array in single expression
+jungle.unshift("lion","buffalo")
+
+puts jungle
 
 # 3. convert your name to an array, and display it, in a single expression
+first = "Cath"
+last = "Rooney"
+
+puts "#{first} #{last}"
 
 # 4. remove and display the last value in the Task # 1 array, and discuss
 # with the instructor why both values added in Task # 2 may have been removed
+lonely_lion = jungle.shift
+puts "removed: #{lonely_lion}"
+puts jungle
--- hashes.rb
+++ hashes.rb
@@ -1,9 +1,14 @@
 # 1. create a hash with variable name states comprised of three state names, including New York, and their abbreviations
+states = { ny: "New York", ca: "California", wa: "Washington State"}
  
 # 2. create second hash with variable named request with url and ip as key names
+web = { url: "www.cathrooney.co.uk", ip: "123456"}
 
 # 3. display the url in the request hash
+puts web[:url]
 
-# 4. display the abbreviation for New York
+#4. display the abbrev for New York
+puts states.key("New York");
 
 # 5. display whether the states hash contains the abbreviation NY
+puts states.has_key?(:ny)
