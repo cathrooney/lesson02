e0e60d087caaa9418b35b25dea8896813379e93d
Cath Rooney <cathrooney@gmail.com>
2012-09-26 19:13:20 +0100

    adding answers

--- variables.rb
+++ variables.rb
@@ -1,9 +1,11 @@
 # 1. declare three variables and assign them odd-integer values in a single expression
-
+one, three, five = 1, 3, 5
 
 # 2. write an expression dividing two of the Task # 1 variables, but displaying the full fractional remainder
 # Hint: turn an integer into a float
-
+puts one.to_f / three.to_f
 
 # 3. declare a name variable, assign it your name, add one of your integer variables to it, and display the result
+name = "Cath"
 
+puts name + " " + five.to_s
